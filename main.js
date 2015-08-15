daefd2c170bcb8d544b98e2ed1d39bb6e67ce2cb
Kristiyan Tsaklev <ktsaklev@gmail.com>
2015-08-15 14:59:56 +0300

    Removed unused code. Added time and station select.

--- main.js
+++ main.js
@@ -1,47 +1,34 @@
 var WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
 var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
+var selectedDate;
+
 function init() {
-	var container = document.querySelector('#calendar');
+	var container = document.querySelector('#datepick');
 	var table = container.querySelector('table');
 	var date = new Date();
 
-// BEGIN DEBUG
-	var custom = document.querySelector('#debug #custom-date');
-	custom.value = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + '-' + date.getMilliseconds();
-	custom.addEventListener('keypress', function(e) {
-		if(e.which !== 13) return;
-		var nums = this.value.split(/[,.\: -\/\\]/);
-		var date = new Date(document.querySelector('#selected-date').innerHTML);
-		date.setHours(nums[0]);
-		date.setMinutes(nums[1]);
-		date.setSeconds(nums[2]);
-		date.setMilliseconds(nums[3]);
-		document.querySelector('#result-codes').innerHTML = (nums[0]<10?'0':'') + nums[0] + ' : ' + (nums[1]<10?'0':'') + nums[1] + ' - ' + genCode(date);
-	});
-// END DEBUG
-
-	showBarcodes(date);
+	setDate(date);
 
 	container.querySelector('#btn-prev')
 		.addEventListener('click', function() {
 		date.setMonth(date.getMonth()-2);
-		fill_table(date, table);
+		fillTable(date, table);
 	})
 	container.querySelector('#btn-next')
 		.addEventListener('click', function() {
 		date.setMonth(date.getMonth());
-		fill_table(date, table);
+		fillTable(date, table);
 	});
 
-	fill_table(date, table);
+	fillTable(date, table);
 	table.addEventListener('click', function(ev) {
 		if(ev.target.className === 'curr-month')
-			showBarcodes(ev.target.id);
+			setDate(ev.target.id);
 	});
 }
 
-function fill_table(date, table) {
+function fillTable(date, table) {
 	document.querySelector('#current-month').innerHTML = MONTHS[date.getMonth()] + ' ' + date.getFullYear();
 	var month = date.getMonth();
 	date.setDate(0);
@@ -70,45 +57,10 @@ function fill_table(date, table) {
 	}
 }
 
-function showBarcodes(date) {
-	date = new Date(date + '');
-	document.querySelector('#selected-date').innerHTML = date.getDate() + ' ' + MONTHS[date.getMonth()] + ' ' + date.getFullYear();
-	var results = document.querySelector('#result-codes');
-	results.innerHTML = '';
-
-	// be cool, use random
-	date.setMilliseconds((Math.random() + '').substr(2, 3));
-	for(var h=0; h<24; ++h)
-	{
-		date.setHours(h);
-		date.setMinutes(0);
-		results.innerHTML += (h<10?'0':'') + h + ' : 00 - ' + genCode(date) + '<br>';
-		date.setMinutes(30);
-		results.innerHTML += (h<10?'0':'') + h + ' : 30 - ' + genCode(date) + '<br>';
-	}
-}
-
-function genCode(date) {
-	var origin = new Date(2014, 2, 6); // 6 March 2014 for some reason
-	origin.setHours(date.getHours());
-	var days = Math.round((date-origin)/86400000) + '';
-
-	var h = date.getHours();
-	var m = date.getMinutes();
-	var s = date.getSeconds();
-	var seconds = (h*60 + m)*60 + s + '';
-	while(seconds.length < 5) seconds = '0' + seconds;
-
-	var millis = date.getMilliseconds() + '';
-	// may be milliseconds, may be a random id, may be someting else, not sure
-	while(millis.length < 3) millis = '0' + millis;
-
-	var code = days + seconds + millis;
-	var checksum = 0;
-	for(var i in code)
-		if((code.length-i)%2)
-			checksum += (code[i]|0)*3;
-		else checksum += code[i]|0;
-	code += checksum*9%10;
-	return code;
+function setDate(date) {
+	selectedDate = new Date(date + '');
+	document.querySelector('#selected-date').innerHTML =
+		selectedDate.getDate() + ' ' +
+		MONTHS[selectedDate.getMonth()] + ' ' +
+		selectedDate.getFullYear();
 }
